9d63af12bbb023c32c6eaf7dffe6e2af3e04f612
Hoang Tran <hoanglam.tran.1.ctr@us.af.mil>
2023-07-17 10:18:37 -0400

    fix upload cache error

--- js/coast.js
+++ js/coast.js
@@ -346,7 +346,7 @@ function checkUploadSpeed(iterations) {
   check();
 
   function check() {
-      var url = '/coast?cache=' + Math.floor(Math.random() * 10000); //prevent url cache
+      var url = '/coast/?cache=' + Math.floor(Math.random() * 10000); //prevent url cache
       var data = getRandomString(1); //1 megabit POST size handled by all servers
       var startTime;
       var speed = 0;
