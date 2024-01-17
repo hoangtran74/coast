6c9cddc841a284dbcb3aeeabb3b09a140c396b40
hoangtran74 <51806327+hoangtran74@users.noreply.github.com>
2024-01-17 15:38:47 -0500

    Update coast.js

--- js/coast.js
+++ js/coast.js
@@ -120,7 +120,7 @@ function testRunning (ev) {
 		percent = ev.loaded / ev.total * 100.0;
 		eta = (ev.total - ev.loaded) / Bps;
 		prev_ev_val = ev.loaded;
-		document.getElementById('dl-stats').innerHTML += count + ": " + Math.round(Bps) + "\tMbps (" + bit + " bits) \tTotal Download: "+bittotal+" (" + Math.round(bittotal/8)+ "MB)\n" ;
+		document.getElementById('dl-stats').innerHTML += count + ": " + bit/1024/1024*8 + " MB \tTotal Download: "+bittotal+" (" + Math.round(bittotal/1024/1024/*8)+ "MB)\n" ;
 	}
     document.getElementById('btnx').style.display = "block";
 	document.getElementById('handle').style.visibility = "visible";
