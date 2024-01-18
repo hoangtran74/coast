414d6106067c4045b44d42cf259dd6adaab228bc
hoangtran74 <51806327+hoangtran74@users.noreply.github.com>
2024-01-17 22:15:20 -0500

    Update coast.js

--- js/coast.js
+++ js/coast.js
@@ -355,7 +355,7 @@ function checkUploadSpeed(iterations) {
   check();
 
   function check() {
-      var url = '/coast-express/?cache=' + Math.floor(Math.random() * 10000); //prevent url cache
+      var url = '/coast-express?cache=' + Math.floor(Math.random() * 10000); //prevent url cache
       var data = getRandomString(1); //1 megabit POST size handled by all servers
       var startTime;
       var speed = 0;
